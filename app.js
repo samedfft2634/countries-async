87383af015e7d198d8f8d0cfe7f24d1bd3d8d63a
samedfft2634 <samedfft@gmail.com>
2023-11-17 12:28:13 +0300

    second commit

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 const searchDiv = document.getElementById("searchDiv");
 const searchInput = document.getElementById("search");
-
-window.addEventListener("load",()=>{
-    searchDiv.classList.add("visually-hidden")  
-})
+let countries = []
 
 const getData = async () => {
-	const response = await fetch(`https://restcountries.com/v3.1/all`);
-	const data = await response.json();
-	console.log(data);
-	getCountryInfo(data);
-    
+    try{
+        const response = await fetch(`https://restcountries.com/v3.1/all`);
+        if(!response.ok){
+            throw new Error(`Something went wrong:${res.status}`) 
+        }
+        const data = await response.json();
+        countries = data
+        showCountry(data[255])
+    } catch (error) {
+        console.log(error)
+    }
+	
 };
-getData();
+window.addEventListener("load",getData);
+searchInput.addEventListener("input",(e)=>{
+    searchDiv.innerHTML = ""
+    if(e.target.value){
+        let filterCountry = countries.filter((item))
+        
+    }
+})
 
-const getCountryInfo = (data) => {
-	data.forEach((x) => {
-		const countryList = document.createElement("span");
-		countryList.classList.add("list");
-		searchDiv.appendChild(countryList);
-		countryList.textContent = x.name.common;
-	});
-    
+const createEl = (span) => {
+	let optionElement = `<span class="list border border-2 rounded-2 p-1" role="button">${span}</span>`;
+    searchDiv.innerHTML += optionElement
 };
 
 //Auto Complete Function
 const getFilter = (e) => {
-    searchDiv.classList.remove("visually-hidden")  
+	searchDiv.classList.remove("visually-hidden");
 	const countryList = document.querySelectorAll(".list");
 	let text = e.target.value;
 	let pat = new RegExp(text, "i");
@@ -34,21 +40,28 @@ const getFilter = (e) => {
 		if (pat.test(country.innerText)) {
 			country.classList.remove("visually-hidden");
 		} else {
-			console.log(country);
+			// console.log(country);
 			country.classList.add("visually-hidden");
 		}
 	});
+	// console.log(pat);
+	return pat;
 };
-searchInput.addEventListener("keyup",()=>{
-    getFilter()
-    showCountry(data);
-});
-
-
+searchInput.addEventListener("keyup", getFilter);
 
 // Show Country Informations On Card
-const showCountry = (data) =>{
-    data.forEach((countries)=>{
-        const { flags, name,region, capital, languages,currencies,population, borders, maps} = countries
-    })
-}
+const showCountry = (country) => {
+		const {
+			name: {common},
+			capital,
+			region,
+			flags: {svg},
+			languages,
+			currencies,
+			population,
+			borders,
+			maps,
+		} = country;
+        console.log(maps.googleMaps)
+	;
+};
